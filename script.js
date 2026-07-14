342617391e9192805dc5029bbc122fb06d28aa69
yashrana20598-alt <yashrana20598@gmail.com>
2026-07-14 18:36:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 npm create vite@latest my-react-app
+function App() {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <p>This is my first React application.</p>
+    </div>
+  );
+}
+
+export default App;
